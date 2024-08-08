31dba897c77ca835c5e8af2b149fbcf1422f41c9
Zuhaib <zuhaibkhoso110786@gmail.com>
2024-08-08 05:14:51 +0500

    Asset Imported

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class FinishPoint2 : MonoBehaviour
 {
     [SerializeField] GameObject LevelComplete;
+  
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
             LevelComplete.SetActive(true);
+            
         }
     }
 
